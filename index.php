a4be3213cdd903a4279353f4509239b790d85b9a
Василий <backahell@rambler.ru>
2013-12-04 23:23:17 +0600

    Исправление репозитория

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('UTC');
 error_reporting(2047);
-ini_set('display_errors', 0);//
+ini_set('display_errors', 0);
 
 
 require_once 'application/bootstrap.php';
