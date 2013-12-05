fa34e6e54d51e246cde2a8866af692fcbc7e4eda
Василий <backahell@rambler.ru>
2013-12-05 09:27:55 +0600

    Исправление репозитория

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -3,4 +3,4 @@ session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] .'/application/core/config/autoloader.php';
 use application\core\config\route;
 $objRoute = new Route();
-$objRoute->Run(); //
+$objRoute->Run();
