f5aa8e54ff380a73e19f0f664e2a6c6b3a6b97ec
Тима <timagima@yandex.ru>
2014-01-11 17:12:02 +0000

    Промо страница

--- skins/tpl/about/promo.tpl.php
+++ skins/tpl/about/promo.tpl.php
@@ -4,7 +4,7 @@
     #internal-slider{width:930px;margin:auto;}
     #social-buttons{position:absolute;right:40px;top:30px;}
     #social-buttons a{margin-left:12px;display:block;float:left;}
-    #select{position:absolute;right:0px;top:172px;}
+    #select{position:absolute;right:1px;top:172px;}
     #prev{display:block;width:61px;height:58px;background:url(/skins/img/prev.png)no-repeat;float:left;}
     #next{display:block;width:56px;height:58px;background:url(/skins/img/next.png)no-repeat;margin-left:60px;}
     #bottom-select{position:absolute;left:45%;top:100%;}
@@ -83,11 +83,10 @@
                 <img src="/skins/img/payment.jpg"/>
             </div>
             <div class="bottom-content">
-                <p>Все условия и правила ближайшего турнира читайте на нашем сайте <a href="/tournament">http://gs11.ru/tournament</a>.
+                <p>Все условия и правила ближайшего турнира читайте на нашем сайте <a href="#">http://gs11.ru/tournament</a>.
                 Участие в турнирах возможно только в том случае, если вы зарегистрировались на сайте <a href="/">GS11</a>. Это дает вам возможность
                 не только участвовать в турнирах, но и вести блоги по пройденным играм, читать новости всей игровой индустрии и многое другое... </p>
             </div>
-
-
+<p>Тра та</p>
         </div>
 </div>
