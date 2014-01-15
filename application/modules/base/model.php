56955549ee959447f34e0a4dbc861270c7d9318c
alexforce2 <alexforce2@yandex.ru>
2014-01-15 16:31:44 +0500

    Реализованы методы просмотра и редактирования пройденных игр

--- application/modules/base/model.php
+++ application/modules/base/model.php
@@ -31,6 +31,7 @@ class Model extends MainModel
     //Добавление пройденной игры в БД
     public function AddCompletedGame()
     {
+        //todo сделать проверку начало и конца прохождения игры, согласно логики времени
         $gameStartDate = $this->_p['game-start-date'];
         $gameEndDate = $this->_p['game-end-date'];
         $gameStartDate = (((int)$gameStartDate) > 0) ? $this->MakeUnixTime(explode("-", $gameStartDate, 3)) : 0;
@@ -38,19 +39,23 @@ class Model extends MainModel
         $idLevelGame = explode("\$", $this->_p['game-level']);
         $idGame = (isset($idLevelGame[1]))?(int)$idLevelGame[1]:0;
         $idLevel = (int)$idLevelGame[0];
+        $questQount = (int)$this->_p['quest-qount'];
+        $idGamePassing = (int)$this->_p['game-passing'];
         $idUser = (int)$_SESSION['user-data']['id'];
         $gameDescription = $this->_p['game-description'];
         $postDate = time();
         $checkAddedGame = $this->conn->dbh->query("SELECT id_game FROM user_completed_games WHERE id_game=" . $idGame . " AND id_user=" . $idUser)->fetch(PDO::FETCH_ASSOC);
         $checkGame = $this->conn->dbh->query("SELECT name FROM games WHERE id = ".$idGame)->fetch(PDO::FETCH_ASSOC);
         if ($checkAddedGame === false && $checkGame !== false) {
-            $stmt = $this->conn->dbh->prepare("INSERT INTO user_completed_games (id_user, id_game, id_level, about_game, start_date, end_date, post_date) VALUES(:idUser, :idGame, :idLevel, :gameDescription, :startDate, :endDate, :postDate)");
+            $stmt = $this->conn->dbh->prepare("INSERT INTO user_completed_games (id_user, id_game, id_level, about_game, start_date, end_date, post_date, num_quest, id_type_completed_game) VALUES(:idUser, :idGame, :idLevel, :gameDescription, :startDate, :endDate, :postDate, :questQount, :idGamePassing)");
             $stmt->bindParam(":idUser", $idUser, PDO::PARAM_INT);
             $stmt->bindParam(":idGame", $idGame, PDO::PARAM_INT);
             $stmt->bindParam(":idLevel", $idLevel, PDO::PARAM_INT);
             $stmt->bindParam(":startDate", $gameStartDate, PDO::PARAM_INT);
             $stmt->bindParam(":endDate", $gameEndDate, PDO::PARAM_INT);
             $stmt->bindParam(":postDate", $postDate, PDO::PARAM_INT);
+            $stmt->bindParam(":questQount", $questQount, PDO::PARAM_INT);
+            $stmt->bindParam(":idGamePassing", $idGamePassing, PDO::PARAM_INT);
             $stmt->bindParam(":gameDescription", $gameDescription, PDO::PARAM_STR);
             $getQuery = $stmt->execute();
             return ("addGame");
@@ -61,15 +66,69 @@ class Model extends MainModel
         }
     }
 
+    //Изменение ранее добавленной игры
+    public function UpdateAddedGame()
+    {
+        //todo сделать проверку начало и конца прохождения игры, согласно логики времени
+        $gameStartDate = $this->_p['game-start-date'];
+        $gameEndDate = $this->_p['game-end-date'];
+        $gameStartDate = (((int)$gameStartDate) > 0) ? $this->MakeUnixTime(explode("-", $gameStartDate, 3)) : 0;
+        $gameEndDate = (((int)$gameEndDate) > 0) ? $this->MakeUnixTime(explode("-", $gameEndDate, 3)) : 0;
+        $idGame = (int)$this->_p['game-id'];
+        $idLevel = (int)$this->_p['level-id'];
+        $questQount = (int)$this->_p['quest-qount'];
+        $idGamePassing = (int)$this->_p['game-passing'];
+        $idUser = (int)$_SESSION['user-data']['id'];
+        $gameDescription = $this->_p['game-description'];
+
+        $stmt = $this->conn->dbh->prepare("UPDATE user_completed_games
+                                            SET id_level=:idLevel, id_type_completed_game=:idGamePassing, num_quest=:questQount, start_date=:startDate, end_date=:endDate, about_game=:gameDescription
+                                            WHERE id_user=:idUser AND id_game=:idGame");
+        $stmt->bindParam(":idUser", $idUser, PDO::PARAM_INT);
+        $stmt->bindParam(":idGame", $idGame, PDO::PARAM_INT);
+        $stmt->bindParam(":idLevel", $idLevel, PDO::PARAM_INT);
+        $stmt->bindParam(":startDate", $gameStartDate, PDO::PARAM_INT);
+        $stmt->bindParam(":endDate", $gameEndDate, PDO::PARAM_INT);
+        $stmt->bindParam(":questQount", $questQount, PDO::PARAM_INT);
+        $stmt->bindParam(":idGamePassing", $idGamePassing, PDO::PARAM_INT);
+        $stmt->bindParam(":gameDescription", $gameDescription, PDO::PARAM_STR);
+        $getQuery = $stmt->execute();
+        return ("GameUpdated");
+    }
+
     //Получение пройденных игр из БД
     public function GetUserCompletedGames()
     {
-        return $this->conn->dbh->query("SELECT ucg.num_quest, ucg.start_date, ucg.end_date, ucg.post_date, ucg.about_game, games.name as game, games.source_img_b, games.source_img_s, level.name as level, level.description as level_description
-                                        FROM user_completed_games ucg, games, level, users
-                                        WHERE ucg.id_user=users.id
-                                        AND ucg.id_game=games.id
-                                        AND ucg.id_level=level.id
-                                        AND users.id=" . $_SESSION['user-data']['id'] . " ORDER BY game")->fetchAll(PDO::FETCH_ASSOC);
+        return $this->conn->dbh->query("SELECT ucg.num_quest, ucg.start_date, ucg.end_date, ucg.post_date, ucg.about_game,
+                                            games.name as game, games.source_img_b, games.source_img_s, games.id,
+                                            level.name as level, level.description as level_description,
+                                            tcg.name as type_complete_game,
+                                            genre.name as genre
+                                        FROM user_completed_games ucg
+                                        LEFT JOIN users ON users.id=ucg.id_user
+                                        LEFT JOIN games ON games.id=ucg.id_game
+                                        LEFT JOIN level ON level.id=ucg.id_level
+                                        LEFT JOIN type_complete_game tcg ON tcg.id=ucg.id_type_completed_game
+                                        LEFT JOIN genre ON genre.id=games.genre_id
+                                        WHERE users.id=" . $_SESSION['user-data']['id'] . " ORDER BY game")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //Получение информации о пройденной игре пользователя по ИД игры
+    public function GetGameView($idGame)
+    {
+        return $this->conn->dbh->query("SELECT ucg.num_quest, ucg.start_date, ucg.end_date, ucg.post_date, ucg.about_game, ucg.id_game,
+                                            games.name as game, games.source_img_b, games.source_img_s,
+                                            level.name as level, level.description as level_description,
+                                            tcg.name as type_complete_game,
+                                            genre.name as genre
+                                        FROM user_completed_games ucg
+                                        LEFT JOIN users ON users.id=ucg.id_user
+                                        LEFT JOIN games ON games.id=ucg.id_game
+                                        LEFT JOIN level ON level.id=ucg.id_level
+                                        LEFT JOIN type_complete_game tcg ON tcg.id=ucg.id_type_completed_game
+                                        LEFT JOIN genre ON genre.id=games.genre_id
+                                        WHERE users.id=" . $_SESSION['user-data']['id'] . " AND ucg.id_game=".$idGame." ORDER BY game ")->fetch(PDO::FETCH_ASSOC);
+
     }
 
     //Получение игр из БД
@@ -82,6 +141,12 @@ class Model extends MainModel
     public function GetTypeCompleteGame()
     {
         return $this->conn->dbh->query("SELECT * FROM type_complete_game")->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //Получение массива уровней сложности игры по её ИД
+    public function GetLevels($idGame)
+    {
+        return $this->conn->dbh->query("SELECT id, name FROM level WHERE id_game=".$idGame)->fetchAll(PDO::FETCH_ASSOC);
     }
 
     //Автокомплит названия игры при наборе в модальной форме
