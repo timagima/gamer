555b9cab1314e2a9bdb2bc2a1a1a146088c2408a
alexforce2 <alexforce2@yandex.ru>
2014-02-08 16:47:23 +0500

    Страница пользователей, которые добавили пройденные игры

---
+++ skins/tpl/base/users.tpl.php
@@ -0,0 +1,23 @@
+<script type="text/javascript" src="/skins/js/validation.js"></script>
+<div id="featured-section" style="position: relative; z-index: 10;" class="FL">
+    <table>
+        <tr>
+            <td style="vertical-align: top;">
+                <div id="content" style="width: 668px;">
+
+                    <div id="content-profile">
+
+                        <div class='left'>
+
+                            <div class="game-content">
+                                <h1>Даныые успешно изменены!</h1>
+                                <?php var_dump($_POST)?>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
