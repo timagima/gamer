33192f94bdd739aaea8cb7e66537a99fd2cc15ec
Тима <timagima@yandex.ru>
2014-02-27 18:55:44 +0000

    Страница благодарности

--- skins/tpl/about/thanks.tpl.php
+++ skins/tpl/about/thanks.tpl.php
@@ -1,55 +1,58 @@
 <style>
-    #main{width:960px;margin:0 auto;overflow:hidden;padding-top:10px;}
+    #main{width:960px;margin:0 auto;padding-top:10px;}
     h2{text-align:center;margin-top:0;}
-    .thanks{position:relative;margin:20px 40px 0 40px;float:left;}
-    .thanks img{cursor:pointer;}
-    .hide{display:none;}
-    .vkontakte, .youtube,.google{font-size:11px; border:1px solid #e9e9e9; position: absolute !important;
-    z-index: 100;  text-align: left; padding: 15px; background-color: white;
-    left:100px;top:-60px;width:200px;height:100px;overflow:hidden;}
+    .thanks{margin:20px 40px 0 40px;float:left;position:relative;}
+    .info-window{font-size:11px; border: 1px solid #e9e9e9; width: 200px; height:200px; position: absolute !important; z-index: 100; left: 100px; bottom:20px; padding: 15px; background-color: white;display:none}
+    .remove:after{content:"Закрыть";position:absolute;left:180px;top:-2px;cursor:pointer;}
+
+
+
 </style>
 <script>
-    function showContent(param){
-        $('.'+param).css('height',200);
-    }
-    function showInfoThanks(param){
-        if(param == "out"){
-            $(".vkontakte,.google,.youtube").css('height','100').hide();
-        }else{
+    $(document).ready(function(){
+        $('#main').on('click','.click-thanks',function(){
+            var id = $(this).siblings('div').attr('id');
             $.ajax({
                 type: 'POST',
                 url: document.location.href,
-                data: {"id": param},
+                data: {"id": id},
                 success: function(data){
                     var result = '';
                     var obj = $.parseJSON(data);
-                    result = "<img src='/storage"+obj['source_img']+"' />" +
-                        "<p>Партнер "+obj['name_partner']+"</p>" +
-                        "<a href='"+obj['link']+"'>Подробнее о "+obj['link_anchor']+"</a>" +
-                        "<p>"+obj['text']+"</p>";
-                    $('.'+param).show().html(result);
+                    result = "<img src='/storage"+obj.source_img+"' />" +
+                        "<p>Партнер "+obj.name_partner+"</p>" +
+                        "<a href='"+obj.link+"'>Подробнее о "+obj.link_anchor+"</a>" +
+                        "<p>"+obj.text+"</p>";
+                    $('#'+id).html(result).show();
                 }
             });
-         }
-   }
 
+
+        });
+        $('.remove').on('click',function(){
+            $('.info-window').hide(500);
+        })
+    });
 </script>
 <div id="main">
 
     <? include $_SERVER['DOCUMENT_ROOT']. '/skins/tpl/block/menu-about.block.tpl.php';?>
     <h2>Хотим выразить огромную благодарнсть нашим партнёрам</h2>
-    <dl class="thanks" onmouseover="showInfoThanks('vkontakte')" onmouseout="showInfoThanks('out')" onclick="showContent('vkontakte')">
-        <dt><img src="/storage/thanks/vk-thanks.png" /></dt>
-        <dd class="vkontakte hide">Тра</dd>
-    </dl>
-    <dl class="thanks" onmouseover="showInfoThanks('youtube')" onmouseout="showInfoThanks('out')" onclick="showContent('youtube')">
-        <dt><img src="/storage/thanks/Yt-thanks.png"  /></dt>
-        <dd class="youtube hide">Тра</dd>
-    </dl>
-    <dl class="thanks" onmouseover="showInfoThanks('google')" onmouseout="showInfoThanks('out')" onclick="showContent('google')">
-        <dt><img src="/storage/thanks/gg-thanks.png"  /></dt>
-        <dd class="google hide">Тра</dd>
-    </dl>
+    <div class="thanks">
+        <img src="/storage/thanks/vk-thanks.png" class="click-thanks" />
+        <div class="info-window remove" id="vkontakte" ></div>
+    </div>
+    <div class="thanks">
+        <img src="/storage/thanks/Yt-thanks.png" class="click-thanks" />
+        <div class="info-window remove" id="youtube" ></div>
+    </div>
+    <div class="thanks">
+        <img src="/storage/thanks/gg-thanks.png"  class="click-thanks" />
+        <div class="info-window remove" id="google" ></div>
+    </div>
+
+
+
 
 
 
