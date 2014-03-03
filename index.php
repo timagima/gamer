53f7bc4ef583ddf0dd01d974d7a57f0f29aa455e
Василий <backahell@rambler.ru>
2014-03-03 16:17:49 +0600

    fixes #3 Проверить issue

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('UTC');
 error_reporting(0);
 ini_set('display_errors', 0);
-require_once 'application/bootstrap.php'; // новые изменения
+require_once 'application/bootstrap.php';
 //echo 'Время выполнения скрипта: ' . (microtime(true) - $start) . ' сек.<br />';
